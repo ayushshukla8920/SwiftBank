39b1f96172635b2bd914a4604ce1f0583a1f6294
Ayush Shukla <149079402+ayushshukla8920@users.noreply.github.com>
2024-12-31 15:32:19 +0530

    Update index.js

--- ApplicationServer/index.js
+++ ApplicationServer/index.js
@@ -16,6 +16,7 @@ const kycRouter = require('./Routes/kyc');
 const depositRouter = require('./Routes/deposit');
 const { startSessionCleanup } = require('./middlewares/cAuth');
 const { startKycCleanup } = require('./middlewares/kyc');
+const session = require('express-session');
 
 connectMongodb(process.env.MONGOURL);
 startSessionCleanup();
@@ -36,7 +37,12 @@ app.options('*', cors({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(session({
+    secret: 's#e$s%s^i*o(n',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
 
 //Routes
