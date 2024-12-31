cb3279446a4f29bc7d509c76c5e5f65e31822bf0
Ayush Shukla <149079402+ayushshukla8920@users.noreply.github.com>
2024-12-31 14:27:35 +0530

    Delete Application Server directory

--- Application Server/Routes/logout.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleAdminLogout,handleClientLogout } = require('../controllers/logout');
-
-router.get('/',handleClientLogout);
-router.get('/admin',handleAdminLogout);
-
-module.exports = router;
