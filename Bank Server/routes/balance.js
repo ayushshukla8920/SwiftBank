a70f9d3217fd114c97298b5f17c1a92e7d48d41b
Ayush Shukla <149079402+ayushshukla8920@users.noreply.github.com>
2025-02-16 04:04:28 +0530

    Delete Bank Server directory

--- Bank Server/routes/balance.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleUserBalance } = require('../controller/balance');
-
-
-router.get('/:customerId',handleUserBalance);
-
-module.exports = router;
