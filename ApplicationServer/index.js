df1d047b4814dd4072c1a15438665eb02e9252b3
Ayush Shukla <149079402+ayushshukla8920@users.noreply.github.com>
2025-02-16 04:17:09 +0530

    Update index.js

--- ApplicationServer/index.js
+++ ApplicationServer/index.js
@@ -21,10 +21,7 @@ connectMongodb(process.env.MONGOURL);
 
 //Middlewares
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 //Public Routes
